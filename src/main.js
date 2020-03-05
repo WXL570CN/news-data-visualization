4af9e4a66c81e104902cea21a549f73a2b245cda
WXL570CN <1812085702@qq.com>
2020-03-05 16:51:20 +0800

    配置 axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+// 引入 axios
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:3000/sinaNews/'
+// 配置axios 的请求拦截器
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
+// 挂载到Vue实例原型上
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
